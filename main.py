6718792b6f59f1e0128c8f3c62709a53400ca995
itz00priya <emo708121@gmail.com>
2026-02-13 16:41:09 +0530

    modify

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-"""
-Entry point for this Python project.
-
-Run this file with:
-
-    python main.py
-"""
-
-
 def main() -> None:
     """Main application function."""
     print("Hello from main.py!")
